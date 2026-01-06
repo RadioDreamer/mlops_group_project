b757716a1ef8bcc6ef047c44d1c737d5389932b5
Buzga Levente <41543192+RadioDreamer@users.noreply.github.com>
2026-01-06 10:49:20 +0100

    feat: Add rules for ruff and bump to py3.13 (#19)

--- src/fakeartdetector/train.py
+++ src/fakeartdetector/train.py
@@ -1,10 +1,12 @@
-from fakeartdetector.model import Model
 from fakeartdetector.data import MyDataset
+from fakeartdetector.model import Model
+
 
 def train():
     dataset = MyDataset("data/raw")
     model = Model()
     # add rest of your training code here
+
 
 if __name__ == "__main__":
     train()
